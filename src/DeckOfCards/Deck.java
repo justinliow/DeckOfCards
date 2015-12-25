77675d52dc190ca5d2e1dc214152339d5a2c3143
justinliow <justinliow@gmail.com>
2015-12-25 01:35:21 -0600

    removed trimtosize, fixed issue with array indexing

--- src/DeckOfCards/Deck.java
+++ src/DeckOfCards/Deck.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class Deck {
 	
-	ArrayList<Card> DeckOfCards;
-	ArrayList<Card> Dealt;
+	ArrayList<Card> DeckOfCards = new ArrayList<Card>();
+	ArrayList<Card> Dealt = new ArrayList<Card>();
 	
 	public Deck()
 	{
-		for(int x=0; x<13; x++){
-			for(int y=0; y<4; y++){
+		for(int x=1; x <= 13; x++){
+			for(int y = 1; y <= 4; y++){
 				Card nCard= new Card(y, x);
 				DeckOfCards.add(nCard);
 			}	
@@ -20,15 +20,15 @@ public class Deck {
 
 	public Card show()
 	{
-		Card topCard=DeckOfCards.get(DeckOfCards.size());
+		Card topCard=DeckOfCards.get(DeckOfCards.size()-1);
 		return topCard;
 	}
 	public Card deal()
 	{
-		Card topCard=DeckOfCards.get(DeckOfCards.size());
+		Card topCard=DeckOfCards.get(DeckOfCards.size()-1);
 		Dealt.add(topCard);
-		DeckOfCards.remove(DeckOfCards.size());
-		DeckOfCards.trimToSize();
+		DeckOfCards.remove(DeckOfCards.size()-1);
+		//DeckOfCards.trimToSize();
 		return topCard;
 	}
 	
@@ -36,7 +36,7 @@ public class Deck {
 	{
 		while(!Dealt.isEmpty())
 		{
-			Card dealtTop=Dealt.remove(Dealt.size());
+			Card dealtTop=Dealt.remove(Dealt.size()-1);
 			Dealt.trimToSize();
 			DeckOfCards.add(dealtTop);
 		}
--- src/DeckOfCards/Test.java
+++ src/DeckOfCards/Test.java
@@ -3,10 +3,14 @@ package DeckOfCards;
 public class Test {
 
 	public static void main(String[] args) {
-		Deck test=new Deck();
-		for(int x = 0; x < test.DeckOfCards.size(); x++)
+		Deck test = new Deck();
+		//System.out.println(test.DeckOfCards.size());
+		int deckSize = test.DeckOfCards.size();
+		for(int x = 0; x < deckSize; x++)
 		{
-			
+			Card tempCard = test.deal();
+			System.out.println("Value: " + tempCard.getValue());
+			System.out.println("Suit: " + tempCard.getSuit());
 		}
 
 	}
